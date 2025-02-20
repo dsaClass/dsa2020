0908875c8a4d6a3792ec1ee5e294fac9e7cce87c
TruElysia1111 <2400010630@stu.pku.edu.cn>
2025-02-20 23:14:49 +0800

    24681

--- 2400010630.py
+++ 2400010630.py
@@ -1,9 +1,15 @@
 n=int(input())
-def stj(n):
+def jump(n):
     if n==1:
         return 1
     elif n==2:
+        return 1
+    elif n==3:
         return 2
+    elif n==4:
+        return 3
+    elif n==5:
+        return 5
     else:
-        return stj(n-1)+stj(n-2)
-print(stj(n))
+        return jump(n-1)+jump(n-3)+jump(n-5)
+print(jump(n))
