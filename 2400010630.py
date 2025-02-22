0779a857f1f15120c1f5143cf0dd30db2d2c8a4c
TruElysia1111 <2400010630@stu.pku.edu.cn>
2025-02-22 23:54:30 +0800

    18250

--- 2400010630.py
+++ 2400010630.py
@@ -1,9 +1,54 @@
-n=int(input())
-def stj(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 2
+import sys
+m=0
+flag=True
+for line in sys.stdin:
+  c=list(map(int,line.strip().split()))
+  if m==0:
+    if flag:
+      flag=False
     else:
-        return stj(n-1)+stj(n-2)
-print(stj(n))
+      print(' ')
+    n=c[0]
+    m=c[1]
+    a=[]
+    b=[]
+    s=n
+    for i in range(n):
+      a.append([i+1])
+      b.append(i+1)
+  else:
+    x=c[0]
+    y=c[1]
+    m-=1
+    if b[x-1]!=b[y-1]:
+      print('No')
+      a[b[x-1]-1]+=a[b[y-1]-1]
+      a[b[y-1]-1]=[]
+      for l in a[b[x-1]-1]:
+        b[l-1]=b[x-1]
+      s-=1
+    else:
+      print('Yes')
+    if m==0:
+      print(s)
+      ma=0
+      for i in range(n):
+        if len(a[i])!=0:
+          ma=i
+      for i in range(n):
+        if len(a[i])!=0:
+          print(i+1,end='')
+          if ma!=i:
+            print(' ',end='')
+          
+          
+      
+  
+
+
+
+
+
+        
+    
+    
