28bd31cfe2107e64e62b072a45bf4d67aa3b5f87
Zhuiy <yyfbjxch@126.com>
2025-02-25 17:57:38 +0800

    push

---
+++ openjudge/24746/2400010836.py
@@ -0,0 +1,32 @@
+class Node:
+	def __init__(self, data, next=None):
+		self.data, self.next = data, next
+class LinkList:
+	def __init__(self,lst):
+		self.head = Node(lst[0])
+		p = self.head
+		for i in lst[1:]:
+			node = Node(i)
+			p.next = node
+			p = p.next
+	def reverse(self):
+		p = self.head
+		q = p.next
+		p.next = None
+		while q != None:
+			u = q.next
+			q.next = p
+			p = q
+			q = u
+		self.head = p
+####
+	def print(self):
+		p = self.head
+		while p:
+			print(p.data, end=" ")
+			p = p.next
+		print()
+a  = list(map(int,input().split()))
+a = LinkList(a)
+a.reverse()
+a.print()
